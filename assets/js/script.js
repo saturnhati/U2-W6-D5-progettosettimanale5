6d0f6a8b4f59657682cb74a4533f22e963265f4f
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-07 17:56:05 +0200

    fixed dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ const changeTheme = (event) => {
         document.querySelector('.hero').classList.replace('light-mode-hero', 'dark-mode-hero')
         document.querySelector('.navbar').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
         document.querySelector('footer').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
-
-
+        document.querySelector('#button-info').classList.replace('light-mode-button', 'dark-mode-button')
     } else {
         document.querySelector('.main').classList.replace('dark-mode', 'light-mode')
         document.querySelector('.hero').classList.replace('dark-mode-hero', 'light-mode-hero')
         document.querySelector('.navbar').classList.replace('dark-mode-navfoot', 'light-mode-navfoot')
-        document.querySelector('footer').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
+        document.querySelector('footer').classList.replace('dark-mode-navfoot', 'light-mode-navfoot')
+        document.querySelector('#button-info').classList.replace('dark-mode-button', 'light-mode-button')
     }
 }
