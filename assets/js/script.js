dbd140d1b82c7b42f23403cd793423e229b67ae5
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-07 17:16:02 +0200

    fixed navbar sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 const changeTheme = (event) => {
     if (event.target.checked) {
         document.querySelector('.main').classList.replace('light-mode', 'dark-mode')
+        document.querySelector('.hero').classList.replace('light-mode-hero', 'dark-mode-hero')
+        document.querySelector('.navbar').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
+        document.querySelector('footer').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
+
+
     } else {
         document.querySelector('.main').classList.replace('dark-mode', 'light-mode')
+        document.querySelector('.hero').classList.replace('dark-mode-hero', 'light-mode-hero')
+        document.querySelector('.navbar').classList.replace('dark-mode-navfoot', 'light-mode-navfoot')
+        document.querySelector('footer').classList.replace('light-mode-navfoot', 'dark-mode-navfoot')
     }
 }
